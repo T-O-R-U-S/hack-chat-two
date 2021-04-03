cab43ea62e80379818b13f084e0acd8cafdab031
T-O-R-U-S <bageliq@protonmail.com>
2021-04-03 15:24:00 +0400

    Got it working! ✅

--- api/socket.js
+++ api/socket.js
@@ -1,3 +1,23 @@
 export default async (req, res) => {
+  let Pusher = require('pusher');
 
+
+  console.log(req.query)
+
+  res
+  .status(200)
+  .send("A-ok!")
+
+  let pusher = new Pusher({
+    appId: "1182518",
+    key: "a992f09a2d49b2381347",
+    secret: "08e6f586507e75889216",
+    cluster: "ap2",
+    useTLS: true
+  })
+
+  pusher.trigger('message-channel', 'message', {
+    user: "jonas",
+    content: "average"
+  })
 };
--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,44 @@
 <script>
-  import 'https://js.pusher.com/5.0/pusher.min.js';
-  import { onMount } from "svelte";
-  export let date;
+  import "https://js.pusher.com/5.0/pusher.min.js";
+
+  let messages = [
+    {
+      user: "idk",
+      content: "idk man",
+    },
+  ];
 
   Pusher.logToConsole = true;
-  
-  let pusher = new Pusher('a992f09a2d49b2381347', {
-      cluster: 'ap2'
-    });
 
-    var channel = pusher.subscribe('my-channel');
-    
-    channel.bind('my-event', function(data) {
-      alert(JSON.stringify(data));
-    });
-
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
+  let pusher = new Pusher("a992f09a2d49b2381347", {
+    cluster: "ap2",
   });
+
+  var channel = pusher.subscribe("message-channel");
+
+  channel.bind("message", function (data) {
+    appendMessage(data);
+  });
+
+  function appendMessage(newData) {
+    messages=[...messages, {
+      user: newData.user,
+      content: newData.content,
+      style: (newData?.style ? newData.style : "")
+    }];
+    console.log(messages);
+  }
 </script>
 
 <main>
-  <ul>
-    
-  </ul>
+  <ol>
+    {#each messages as message}
+    <li>
+      <p style={message.style}>
+        {message.user} says :: {message.content}
+      </p>
+    </li>
+      
+    {/each}
+  </ol>
 </main>
