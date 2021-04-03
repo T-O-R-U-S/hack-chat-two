027b3235ec5119ca349bcb569ee0aa8577a6c89e
T-O-R-U-S <bageliq@protonmail.com>
2021-04-03 14:56:16 +0400

    Socket.IO is not viable for serverless application -- switched to Pusher.

--- api/socket.js
+++ api/socket.js
@@ -1,8 +1,3 @@
-module.exports = async (req, res) => {
-  const server = require("http").createServer();
-  const IO = require("socket.io")(server);
-  
-  IO.on("connection", (socket) => {
-    console.log(socket);
-  });
+export default async (req, res) => {
+
 };
--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,19 @@
 <script>
+  import 'https://js.pusher.com/5.0/pusher.min.js';
   import { onMount } from "svelte";
   export let date;
+
+  Pusher.logToConsole = true;
+  
+  let pusher = new Pusher('a992f09a2d49b2381347', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    
+    channel.bind('my-event', function(data) {
+      alert(JSON.stringify(data));
+    });
 
   onMount(async () => {
     const res = await fetch("/api/date");
@@ -10,39 +23,7 @@
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <ul>
+    
+  </ul>
 </main>
